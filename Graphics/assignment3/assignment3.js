941a73b2f8840e142f3740dbb41700f0cf7892c3
Grady Hilgendorf <rockenswim@yahoo.com>
2017-10-24 13:10:35 -0500

    mod 12
    motion done, golly g wiz points done, no wire wraping

--- Graphics/assignment3/assignment3.js
+++ Graphics/assignment3/assignment3.js
@@ -10,56 +10,67 @@ var radius = 4.0;		// Used to establish eye point
 var theta  = 0.0;		// Used to establish eye point
 var phi    = 0.0;		// Used to establish eye point
 var rotation_by_5_deg = 5.0 * Math.PI/180.0;
+var rotY = 0.0;
+var transY = 0.0;
+var transX = 0.0;
 
 var  fovy = 45.0;  // Field-of-view in Y direction angle (in degrees)
 var  aspect;       // Viewport aspect ratio
 
 var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
+
+var width;
+var height;
+
 var eye;			// Established by radius, theta, phi as we move
 const at = vec3(0.0, 0.0, 0.0);
 const up = vec3(0.0, 1.0, 0.0);
 
+var fColor;
+
+const black = vec4(0.0, 0.0, 0.0, 1.0);
+const red = vec4(1.0, 0.0, 0.0, 1.0);
+const white = vec4(0.0, 0.0, 0.0, 0.0);
+const blue = vec4(0.0,0.0,1.0, 1.0);
+const green = vec4(0.0,1.0,0.0,1.0);
+var color=false;
+
 ////////////////// Object 1 vertex information //////////////////
 
-// numVerticesObj1, pointsArray1, vertices1, coordsForObj1 are all
-// used to generate the vertex information for "Object 1".  In the
-// assignment, you are required to make this object a more interesting
-// mathematically defined object such as the sombrero surface or
-// Moebius band
+var nRows = 25;
+var nColumns = 25;
 
-var numVerticesObj1  = 36;	// For the 12 triangles
+var datax = [];
+var datay = [];
+var dataz = [];
 
 var pointsArray1 = [];
 
-var vertices1 = [
-    vec4(-0.5, -0.5,  1.5, 1.0),
-    vec4(-0.5,  0.5,  1.5, 1.0),
-    vec4(0.5,  0.5,  1.5, 1.0),
-    vec4(0.5, -0.5,  1.5, 1.0),
-    vec4(-0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5,  0.5, 0.5, 1.0),
-    vec4(0.5,  0.5, 0.5, 1.0),
-    vec4( 0.5, -0.5, 0.5, 1.0)
-];
-
 function coordsForObj1()
 {
-    function quad(a, b, c, d) {
-	pointsArray1.push(vertices1[a]);
-	pointsArray1.push(vertices1[b]);
-	pointsArray1.push(vertices1[c]);
-	pointsArray1.push(vertices1[a]);
-	pointsArray1.push(vertices1[c]);
-	pointsArray1.push(vertices1[d]);
-    };
+  for( var i = 0; i <= nRows; ++i ) {
+    datax.push( [] );
+    datay.push( [] );
+    dataz.push( [] );
+    var u = 2.0 * Math.PI * (i/nRows);
 
-    quad( 1, 0, 3, 2 );
-    quad( 2, 3, 7, 6 );
-    quad( 3, 0, 4, 7 );
-    quad( 6, 5, 1, 2 );
-    quad( 4, 5, 6, 7 );
-    quad( 5, 4, 0, 1 );
+    for( var j = 0; j <= nColumns; ++j ) {
+      var v = -0.3 + ((j/nColumns) * 0.6);
+      datax[i].push(Math.cos(u) + v * Math.sin(u/2.0) * Math.cos(u));
+      datay[i].push(Math.sin(u) + v * Math.sin(u/2.0) * Math.sin(u));
+      dataz[i].push(v * Math.cos(u/2.0));
+    }
+  }
+
+  for(var i=0; i<nRows; i++) {
+    for(var j=0; j<nColumns;j++) {
+      pointsArray1.push( vec4(datax[i][j], datay[i][j], dataz[i][j],1.0));
+      pointsArray1.push( vec4(datax[i+1][j], datay[i+1][j], dataz[i+1][j], 1.0));
+      pointsArray1.push( vec4(datax[i+1][j+1], datay[i+1][j+1], dataz[i+1][j+1], 1.0));
+      pointsArray1.push( vec4(datax[i][j+1], datay[i][j+1], dataz[i][j+1], 1.0));
+    }
+  }
 }
 
 ///////// End of vertex information for Object 1  ////////
@@ -69,7 +80,7 @@ var pointsArray2 = [];
 
 function getBucky(){
   var vert = 0;
-  for(var i=0;i<=240;i++){//Hexagons
+  for(var i=0;i<75;i++){//Hexagons
     if(vert==0){
       pointsArray2.push(buckyBall[i]);
       vert++;
@@ -88,8 +99,12 @@ function getBucky(){
     }
   }
   vert = 0;
-  for(var p=241;p<buckyBall.length-1;p++){
-    if(vert == 0 | vert == 1 | vert == 2 | vert == 5 | vert == 7){
+  for(var p=75;p<buckyBall.length;p++){
+    if(vert == 0){
+      pointsArray2.push(buckyBall[i]);
+      vert++;
+    }
+    else if(vert == 1 | vert == 2 | vert == 5 | vert == 7){
       pointsArray2.push(buckyBall[i]);
       vert++;
     }
@@ -108,18 +123,19 @@ window.onload = function init() {
 
     canvas = document.getElementById( "gl-canvas" );
 
-    //    gl = WebGLUtils.setupWebGL( canvas );
-    gl = WebGLDebugUtils.makeDebugContext( canvas.getContext("webgl") ); // For debugging
+       gl = WebGLUtils.setupWebGL( canvas );
+    // gl = WebGLDebugUtils.makeDebugContext( canvas.getContext("webgl") ); // For debugging
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     aspect =  canvas.width/canvas.height;
-    gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
+    gl.clearColor( Math.random(), Math.random(), Math.random(), 1.0 );
     gl.enable(gl.DEPTH_TEST);
 
     gl.depthFunc(gl.LEQUAL)
     gl.enable(gl.POLYGON_OFFSET_FILL);
-    gl.polygonOffset(1.0,5.0);
+    gl.polygonOffset(1.0,1.0);
+
     //
     //  Load shaders and initialize attribute buffers
     //
@@ -151,12 +167,28 @@ window.onload = function init() {
     document.getElementById("Button6").onclick = function(){theta -= rotation_by_5_deg;};
     document.getElementById("Button7").onclick = function(){phi += rotation_by_5_deg;};
     document.getElementById("Button8").onclick = function(){phi -= rotation_by_5_deg;};
+    document.getElementById("master").onclick = function(){if(color){color=false;}else{color=true;}};
 
     render();
 };
 
+var x=0;
+
 
 var render = function(){
+  if(color){
+    if(x===0){
+      gl.clearColor( Math.random(), Math.random(), Math.random(), 1.0 );
+      x++;
+    }
+    else if(x==15){
+      x=0;
+    }
+    else {
+      x++;
+    }
+  }
+
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
@@ -171,31 +203,43 @@ var render = function(){
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    // gl.uniform4fv(gl.getUniformLocation(program, "fColor"),
-		//   flatten(vec4(1.0, 0.0, 0.0, 1.0)));
-    // gl.drawArrays( gl.TRIANGLES, 0, numVerticesObj1 );
-    //
-    // gl.uniform4fv(gl.getUniformLocation(program, "fColor"),
-		//   flatten(vec4(0.0, 0.0, 0.0, 0.0)));
-    // gl.drawArrays( gl.LINES, 0, numVerticesObj1 );
+    for(var i=0; i<pointsArray1.length; i+=4) {
+        gl.uniform4fv(gl.getUniformLocation(program, "fColor"), flatten(green));
+        gl.drawArrays( gl.TRIANGLE_FAN, i, 4 );
+        gl.uniform4fv(gl.getUniformLocation(program, "fColor"), flatten(black));
+        gl.drawArrays( gl.LINE_LOOP, i, 4 );
+    }
 
 
     // The BuckyBall
     modelViewMatrix = lookAt(eye, at , up);
-    modelViewMatrix = mult(modelViewMatrix, translate(1.75,0.0,0.0));
+    rotY += 2.0;
+    if(Math.cos(transY)==0){
+      transY = 0.0;
+    }
+    if(Math.sin(transX)==1){
+      transX = 1;
+    }
+    else{
+      transY+=0.05;
+      transX-=0.01;
+    }
+
+
+    modelViewMatrix = mult(modelViewMatrix, translate(Math.tan(transX),Math.cos(transY),0.0));
+    // modelViewMatrix = mult(modelViewMatrix, translate(1.75,0.0,0.0));
+    modelViewMatrix = mult(modelViewMatrix, rotateY(rotY));
     modelViewMatrix = mult(modelViewMatrix, scalem(0.03,0.03,0.03));
     projectionMatrix = perspective(fovy, aspect, near, far);
 
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    gl.uniform4fv(gl.getUniformLocation(program, "fColor"),
-		  flatten(vec4(0.0, 0.0, 1.0, 1.0)));
-    gl.drawArrays( gl.TRIANGLES, numVerticesObj1, buckyBall.length);
+    gl.uniform4fv(gl.getUniformLocation(program, "fColor"),flatten(black));
+    gl.drawArrays( gl.TRIANGLES, pointsArray1.length, buckyBall.length);
 
-    gl.uniform4fv(gl.getUniformLocation(program, "fColor"),
-		  flatten(vec4(0.0, 0.0, 0.0, 0.0)));
-    gl.drawArrays(gl.LINE_LOOP,0,pointsArray2.length );
+    gl.uniform4fv(gl.getUniformLocation(program, "fColor"),flatten(white));
+    gl.drawArrays(gl.TRIANGLES, pointsArray1.length, pointsArray2.length );
 
     requestAnimFrame(render);
 };
